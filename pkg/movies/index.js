bb23ccedb83015b1cb386d1e17148a81dead3986
Vangel22 <h.vangel22@gmail.com>
2023-09-07 21:11:55 +0200

    models done

--- pkg/movies/index.js
+++ pkg/movies/index.js
@@ -2,37 +2,56 @@ const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
   title: {
-    // type
-    // required
-    // trim
+    type: String,
+    required: true,
   },
   genre: {
-    // type
-    // required
-    // trim
+    type: String,
+    required: true,
   },
   releaseYear: {
-    // type
-    // required
-    // min
-    // max
+    type: String,
+    required: true,
+    min: 1900,
+    max: new Date().getFullYear(),
   },
   //   director: {
   //      Optional, later to update the accounts model with a property director: true/false
   //   },
   account_id: {
-    // type
-    // ref
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "accounts",
   },
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
 
 //Add methods here - don't forget multitenancy i.e account_id
-const create = () => {};
-const update = () => {};
-const remove = () => {};
-const getAll = () => {};
-const getSingle = () => {};
+const getAll = async (account_id) => {
+  return await Posts.find({ account_id });
+};
 
-module.exports = Movie;
+const getSingle = async (account_id, id) => {
+  return await Movie.findOne({ account_id, _id: id });
+};
+
+const create = async (data) => {
+  const post = new Movie(data);
+  return await post.save();
+};
+
+const update = async (id, data) => {
+  return await Movie.updateOne({ _id: id }, data);
+};
+
+const remove = async (id) => {
+  return await Movie.deleteOne({ _id: id });
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  create,
+  update,
+  remove,
+};
